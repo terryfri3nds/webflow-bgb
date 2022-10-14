6f85fcc8a3611323a0ad402fe5549be551f0f713
Elterry <federico@fri3nds.com>
2022-10-14 21:09:24 +0000

    demo

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-
 /* 
-    INJECT IN CUSTOM CODE
+    INJECT IN CUSTOM CODES
 */
 let myScript = document.createElement("script");
 
-if (window.location.hostname.endsWith('.webflow.io')) //DEVELOPMNENT
-    myScript.setAttribute("src", "https://cgfrse-8080.preview.csb.app/dist/js/pages/home/index.js");
-else//PRODUCTION
-		myScript.setAttribute("src", "https://cdn.jsdelivr.net/gh/terryfri3nds/webflow-tam@v0.1.0/dist/js/pages/home/index.js");
+if (window.location.hostname.endsWith(".webflow.io"))
+  //DEVELOPMNENT
+  myScript.setAttribute(
+    "src",
+    "https://cgfrse-8080.preview.csb.app/dist/js/pages/home/index.js"
+  );
+//PRODUCTION
+else
+  myScript.setAttribute(
+    "src",
+    "https://cdn.jsdelivr.net/gh/terryfri3nds/webflow-tam@v0.1.0/dist/js/pages/home/index.js"
+  );
 
 document.body.appendChild(myScript);
-
